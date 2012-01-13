3d40e6dc9fbbafc88887544dc7305bd5e848c9de
masaka <masaka@gmail.com>
2012-01-13 15:32:01 +0900

    Update check_log_and_send_email.sh

--- check_log_and_send_email.sh
+++ check_log_and_send_email.sh
@@ -5,18 +5,20 @@ check_str='your_check_str'
 mv new old
 grep $check_str your_logfile > new
 
-diff old new
+diff old new > diff_result.txt
 
 if  [ $? -eq 1 ]  ; then
   sendmail -oi -t << EOF
 From: [your_email]
 To: [your_email]
-Subject: ALERT $check_str
+Subject: updated `date +"%Y/%m/%d %p %I:%M:%S"`
 
-`date`
+`cat diff_result.txt`
 EOF
 
    echo 'send a email'
 else 
    echo 'nothing'
 fi
+
+rm diff_result.txt
